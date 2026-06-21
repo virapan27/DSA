8d2ea3d520805bfd7ad0e0e7d642df195b2007f0
Abhishek Mohanty <abhishekmohanty9938@gmail.com>
2026-06-21 20:59:58 +0530

    Time: 3 ms (87.54%) | Memory: 43.1 MB (84.84%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if (top != '(' && ch == ')'){
+                    return false;
+                }
+                if (top != '[' && ch == ']'){
+                    return false;
+                }
+                if (top != '{' && ch =='}'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
