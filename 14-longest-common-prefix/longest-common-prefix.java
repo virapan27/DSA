b7812cafde303a1c3ab4470c712a125a73a50369
Abhishek Mohanty <abhishekmohanty9938@gmail.com>
2026-06-24 00:55:21 +0530

    Time: 1 ms (62.49%) | Memory: 43 MB (82.37%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,20 +1,37 @@
 class Solution {
+    //Approach 1
+    // public String longestCommonPrefix(String[] strs) {
+    //     if (strs == null || strs.length == 0) {
+    //         return "";
+    //     }
+
+    //     String prefix = strs[0];
+    //     for(int i = 1; i <= strs.length - 1; i++) {
+    //         while(!strs[i].startsWith(prefix)) {
+    //             prefix = prefix.substring(0, prefix.length() - 1);
+
+    //             if (prefix.length() == 0) {
+    //                 return "";
+    //             }
+    //         }
+    //     }
+    //     return prefix;
+
+    // }
+
+    //approach 2
+
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
+        String first = strs[0];
+        for(int i = 0; i < first.length(); i++){
+            char current = first.charAt(i);
 
-        String prefix = strs[0];
-        for(int i = 1; i <= strs.length - 1; i++) {
-            while(!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-
-                if (prefix.length() == 0) {
-                    return "";
+            for(int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || current != strs[j].charAt(i)){
+                    return first.substring(0, i);
                 }
             }
         }
-        return prefix;
-
+        return first;
     }
 }
